f7c42f8b6dbab70271a58835a260c635dda3f917
nickbohm5280 <148266554+nickbohm5280@users.noreply.github.com>
2023-10-26 12:40:00 -0400

    Update Game.java

--- Game.java
+++ Game.java
@@ -1,5 +1,9 @@
 public class Game{
+   
     private InputOutput ioDevice;
+    public Game(){
+        ioDevice = new InputOutput();
+    }
     public Move getMove(Player player) {
         Move move;
         ioDevice.print("It is "+player.getName()+"'s move: ");
