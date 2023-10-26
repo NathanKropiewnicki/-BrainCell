d95b71a088de7a7aff3b42aeac3c77a5bd8af01c
nickbohm5280 <148266554+nickbohm5280@users.noreply.github.com>
2023-10-26 12:39:22 -0400

    Update Board.java

--- Board.java
+++ Board.java
@@ -1,13 +1,34 @@
 public class Board{
+ protected Piece[][] myBoard;
 
  public Board() {
-        myBoard = new Piece[DEFAULTSIZE][DEFAULTSIZE];
-        initBoard();
+        myBoard = new Piece[3][3];
+        initBoard();       
     }
   public Board(int size) {
         myBoard = new Piece[size][size];
         initBoard();
     }
+    protected void initBoard() {
+        int row, column;
+        
+        for (row = 0; row < myBoard.length; row++)
+            for (column = 0; column < myBoard[0].length; column++) 
+                myBoard[row][column] = new Piece(Piece.BLANK);
+    }
+    public boolean legalMove(Move move) {
+        int row, column;
+        
+        row = move.getRow();
+        column = move.getColumn();
+        if (row < 0) return false;
+        if (column < 0) return false;
+        if (row >= myBoard.length) return false;
+        if (column >= myBoard.length) return false;
+        return true;
+    }
+
+
     
     public String toString() {
         int row, column;
