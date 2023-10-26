ccaf049ba4276045e67b7d094d9c920870294478
nickbohm5280 <148266554+nickbohm5280@users.noreply.github.com>
2023-10-26 12:41:04 -0400

    Update GameDrive.java

--- GameDrive.java
+++ GameDrive.java
@@ -1,6 +1,6 @@
 import java.io.*;
 import java.util.*;
-public class GameDrive {
+public class GameDriver {
     public static void main(String[] args) {
         TicTacToeGame myGame;
         InputOutput scanner;
