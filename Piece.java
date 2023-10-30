613684c4a999a01785c3123e359b01239b62e305
nickbohm5280 <148266554+nickbohm5280@users.noreply.github.com>
2023-10-30 00:23:13 -0400

    Update Piece.java

--- Piece.java
+++ Piece.java
@@ -1,7 +1,18 @@
+/****************************************
+/* Author: Teacher
+/* Course: CSC 221, Fall 2023
+/* Lab: Inheritance Group Lab
+/* Modified: October 29th 2023
+/* Modified by Nick Bohm
+*****************************************/
+
 public class Piece {
     public static final char BLANK = ' ';
     public static final char X = 'X';
     public static final char O = 'O';
+    public static final char B = 'B';
+    public static final char R = 'R';
+    public static final char W = 'W';
     private char type;
     
     public Piece() {
@@ -13,6 +24,9 @@ public class Piece {
     }
     
     public char getPiece() {
+        return type;
+    }
+    public char getPiece(char inType) {
         return type;
     }
     
@@ -27,5 +41,21 @@ public class Piece {
     public String toString() {
         if (type == BLANK) return "_";
         else return type+"";
+    }
+    public char getOpponentPiece() {
+        switch (type) {
+            case X:
+                return O;
+            case O:
+                return X;
+            case B:
+                return W;
+            case W:
+                return B;
+            case R:
+                return R;
+            default:
+                return BLANK;
+        }
     }
 }
