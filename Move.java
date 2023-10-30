c747c509998469a8cc3943e6f3d8c6292f441152
nickbohm5280 <148266554+nickbohm5280@users.noreply.github.com>
2023-10-30 00:27:28 -0400

    Update Move.java

--- Move.java
+++ Move.java
@@ -1,3 +1,11 @@
+/****************************************
+/* Author: Teacher
+/* Course: CSC 221, Fall 2023
+/* Lab: Inheritance Group Lab
+/* Modified: October 29th 2023
+/* Modified by Nick Bohm
+*****************************************/
+
 public class Move {
     int row, column;
     Player player;
@@ -23,5 +31,10 @@ public class Move {
     
     public String toString() {
         return "("+row+","+column+")";
+    }
+      public Move(int column) {
+        this.column = column;
+        this.player = player;
+
     }
 }
